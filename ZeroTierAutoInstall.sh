4f30665a57d4145dc2c70a182fc08a0e1e041e5e
Diego2316 <57542475+Diego2316@users.noreply.github.com>
2024-01-09 13:32:33 -0500

    Update ZeroTierAutoInstall.sh

--- ZeroTierAutoInstall.sh
+++ ZeroTierAutoInstall.sh
@@ -17,7 +17,7 @@ echo "/usr/sbin/zerotier-one -d" > /etc/s6-overlay/s6-rc.d/zerotier-one/up
 touch /etc/s6-overlay/s6-rc.d/user/contents.d/zerotier-one
 echo "Joining $NETWORKID network"
 zerotier-cli join d5e5fb65374eef97
-echo "Joined network, unauthorization"
+echo "Joined network, unauthorized"
 MYID=$(zerotier-cli info | cut -d " " -f 3)
 JSON=`curl -s -H "Authorization: Bearer $APIKEY" $APIURL/network/$NETWORKID/member/$MYID | jq -c --arg name "Pi-hole" '.config.authorized=true | .config.ipAssignments=["10.147.20.100"] | .name=$name'`
 curl -s -o /dev/null -H "Authorization: Bearer $APIKEY" -d $JSON $APIURL/network/$NETWORKID/member/$MYID
