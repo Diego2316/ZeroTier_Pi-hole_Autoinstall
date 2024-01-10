f9035b29f429f2544653228cf54050a84d017a53
Diego2316 <57542475+Diego2316@users.noreply.github.com>
2024-01-10 09:09:26 -0500

    Update ZeroTierAutoInstall.sh

--- ZeroTierAutoInstall.sh
+++ ZeroTierAutoInstall.sh
@@ -19,7 +19,9 @@ echo "Joining $NETWORKID network"
 zerotier-cli join $NETWORKID
 echo "Joined network, unauthorized"
 MYID=$(zerotier-cli info | cut -d " " -f 3)
-JSON='{"name": "Pi-hole", "config": {"authorized":true, "ipAssignments": ["10.147.20.100"]}}'
-curl -X POST -H 'Content-Type: application/json' -H "Authorization: token $APIKEY" "$APIURL/network/$NETWORKID/member/$MYID" -d '{"name": "Pi-hole", "config": {"authorized":true, "ipAssignments": ["10.147.20.100"]}}'
+curl -s -X POST -H 'Content-Type: application/json' -H "Authorization: token $APIKEY" "$APIURL/network/$NETWORKID/member/$MYID" -d '{"name": "Pi-hole", "config": {"authorized":true, "ipAssignments": ["10.147.20.100"]}}'
+echo -ne "waiting for network auth to register"
+while [ -z "$(zerotier-cli listnetworks | grep $NETWORKID | grep OK)" ]; do echo -ne ".";sleep 1 ; done
+echo -ne '\n'
 MYIP=`zerotier-cli get $NETWORKID ip`
 echo "User $MYID authorized in $NETWORKID network with ip: $MYIP"
